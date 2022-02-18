cab5f0ecac4e969e2e8e4945feee7a0a359cb223
dnsh87 <dnsh.jakhar@gmail.com>
2022-02-18 12:37:17 +0530

    2 files modified, 1 added

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,8 @@ import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['app.component.css'],
 })
 export class AppComponent {
   collapsedNav: boolean;
